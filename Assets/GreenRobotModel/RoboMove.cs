87e030ef5821758ca0bbce7a74b43b0066bb4885
Oxnack <128256361+Oxnack@users.noreply.github.com>
2024-10-29 21:00:13 +0300

    build update

--- Assets/GreenRobotModel/RoboMove.cs
+++ Assets/GreenRobotModel/RoboMove.cs
@@ -17,35 +17,71 @@ public class RoboMove : MonoBehaviour
         rb = GetComponent<Rigidbody>();
     }
 
+    //private IEnumerator MoveHook(float time, int z, int x, int jump)
+    //{
+    //    Rigidbody RoboRb = _robot.GetComponent<Rigidbody>();
+    //    //Assets\GreenRobotModel\RoboMove.cs
+    //    float moveHorizontal = 0;
+    //    float moveVertical = 0;
+
+    //    if (x == -1 || x == 1)
+    //    {
+    //        moveHorizontal = x;
+    //    }
+
+    //    if (z == -1 || z == 1)
+    //    {
+    //        moveVertical = z;
+    //    }
+
+    //    if (jump == 1) {
+    //        // Jump();
+    //        rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+    //    }
+        
+
+    //    //#####
+
+    //    Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical).normalized;
+
+    //    RoboRb.MovePosition(RoboRb.position + movement * _speed * Time.fixedDeltaTime);
+
+    //    yield return new WaitForSeconds(time);
+    //}
+
     private IEnumerator MoveHook(float time, int z, int x, int jump)
     {
         Rigidbody RoboRb = _robot.GetComponent<Rigidbody>();
-        //Assets\GreenRobotModel\RoboMove.cs
-        float moveHorizontal = 0;
-        float moveVertical = 0;
+
+        Vector3 movement = Vector3.zero;
 
         if (x == -1 || x == 1)
         {
-            moveHorizontal = x;
+            movement += new Vector3(x, 0, 0); 
         }
 
         if (z == -1 || z == 1)
         {
-            moveVertical = z;
+            movement += new Vector3(0, 0, z); 
         }
 
-        if (jump == 1) {
-            // Jump();
+        if (movement != Vector3.zero)
+        {
+            movement.Normalize();
+        }
+
+        if (jump == 1)
+        {
             rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
         }
-        
 
-        //#####
+        float elapsedTime = 0;
 
-        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical).normalized;
-
-        RoboRb.MovePosition(RoboRb.position + movement * _speed * Time.fixedDeltaTime);
-
-        yield return new WaitForSeconds(time);
+        while (elapsedTime < time)
+        {
+            RoboRb.MovePosition(RoboRb.position + movement * _speed * Time.fixedDeltaTime);
+            elapsedTime += Time.fixedDeltaTime; 
+            yield return new WaitForFixedUpdate(); 
+        }
     }
 }
