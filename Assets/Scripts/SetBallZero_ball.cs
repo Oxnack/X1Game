ab03c6c97d20833b9f11ab7fcaeee3cab8eaefb4
Oxnack <128256361+Oxnack@users.noreply.github.com>
2025-07-02 00:18:03 +0300

    bg_football_move_and_fix

---
+++ Assets/Scripts/SetBallZero_ball.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using Mirror;
+
+public class SetBallZero_ball : NetworkBehaviour
+{
+    [SerializeField] private Vector3 _ballPosition = new Vector3(0,0,0);
+
+    private GameObject _ball;
+    private void OnTriggerEnter(Collider other)
+    {
+        if (isServer)
+        {
+            if (other.gameObject.CompareTag("Ball"))
+            {
+                _ball = GameObject.FindGameObjectWithTag("Ball");
+                if (_ball != null )
+                {
+                    _ball.transform.position = _ballPosition;
+                    _ball.GetComponent<Rigidbody>().velocity = Vector3.zero;
+                    _ball.GetComponent<Rigidbody>().angularVelocity = Vector3.zero;
+                }
+            }
+        }
+    }
+}
